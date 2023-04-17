13c454b7c53c0441c212d8c715c026e617292df8
wangzhaolun <62985503+wangzhaolun@users.noreply.github.com>
2023-04-17 23:17:01 +0800

    Update About.vue

--- src/components/common/Setting/About.vue
+++ src/components/common/Setting/About.vue
@@ -41,6 +41,9 @@ onMounted(() => {
 <template>
   <NSpin :show="loading">
     <div class="p-4 space-y-4">
+		 <h2 class="text-xl font-bold">
+        Version - {{ pkg.version }}
+      </h2>
       <p>{{ $t("setting.api") }}：{{ config?.apiModel ?? '-' }}</p>
       <p v-if="isChatGPTAPI">
         {{ $t("setting.monthlyUsage") }}：{{ config?.usage ?? '-' }}
