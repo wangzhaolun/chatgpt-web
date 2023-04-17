94e452b80fe62e2530701307086f9e1d54a66d69
wangzhaolun <62985503+wangzhaolun@users.noreply.github.com>
2023-04-17 23:03:34 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://p.ssl.qhimg.com/t01f6b645876dcfe3cb.jpg',
+      name: 'linghang',
+      description: '',
     },
   }
 }
